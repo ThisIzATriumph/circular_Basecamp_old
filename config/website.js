bc5dfe73cb225a39e591e9b88a784fc990e21a9a
ThisIzATriumph <80205784+ThisIzATriumph@users.noreply.github.com>
2021-05-02 16:47:09 +0200

    config files update

--- config/website.js
+++ config/website.js
@@ -1,31 +1,31 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'Maggie Appleton', // Navigation and Site Title
+  siteTitle: 'Circular Basecamp', // Navigation and Site Title
   siteTitleAlt:
-    'Maggie Appleton makes visual explanations about technology, culture, and programming', // Alternative Site title for SEO
-  siteTitleShort: 'Maggie_Appleton', // short_name for manifest
+    'Circular Basecamp gears up designers to support the transition towards a Circular Economy', // Alternative Site title for SEO
+  siteTitleShort: 'Circular_Basecamp', // short_name for manifest
   siteUrl: 'https://maggieappleton.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/favicon-ma.svg', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription:
     'Maggie Appleton is an art director and anthropological designer. This is her digital garden for growing visual explanations about technology, culture, and programming',
-  author: 'Maggie Appleton', // Author for schemaORGJSONLD
-  organization: 'Maggie Appleton',
+  author: 'Baptiste Sene, based on Maggie Appleton s work', // Author for schemaORGJSONLD
+  organization: 'Basendo',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@mappletons', // Twitter Username
-  ogSiteName: 'Maggie Appleton', // Facebook Site Name
+  userTwitter: '@_basendo', // Twitter Username
+  ogSiteName: 'Baptiste Sene', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: 'UA-45097160-1',
 
-  // Manifest and Progress color
-  themeColor: '#FD9C4B',
+  // Manifest and Progress color - Original #FD9C4B
+  themeColor: '#2AAB9F',
   backgroundColor: '#FFFDFD',
 
   // Social component
-  twitter: 'https://twitter.com/mappletons/',
-  twitterHandle: '@mappletons',
-  github: 'https://github.com/maggieappleton/',
-  dribbble: 'https://dribbble.com/mappleton',
-  instagram: 'https://www.instagram.com/maggieappleton/',
+  twitter: 'https://twitter.com/senebapt/',
+  twitterHandle: '@_basendo',
+  github: 'https://github.com/thiisizatriumph/',
+  dribbble: 'https://dribbble.com/thisizatriumph',
+  instagram: 'https://www.instagram.com/circular_basecamp/',
 }
