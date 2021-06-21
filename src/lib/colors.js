b16a1d8a0c4ca119cbbcd85da0aa39b9c35d3437
ThisIzATriumph <80205784+ThisIzATriumph@users.noreply.github.com>
2021-06-21 23:27:22 +0200

    color change

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -6,14 +6,16 @@ const colors = {
   grey: '#686F94',
   lightGrey: '#C5C9DB',
   lightestGrey: '#E0E9F1',
-  orange: '#ed7842',
-  lightOrange: '#F98B3B',
+  orange: '#008080',
+  lightOrange: '#40e0d0',
   blue: '#00A1E3',
   lightBlue: '#22BEF7',
   darkBlue: '#2F73E6',
   lightGreen: '#AAC339',
   navy: '#192C4C',
   red: '#FD5351',
+  //orange: '#ed7842',
+  // lightOrange: '#F98B3B',
 }
 
 export default colors
