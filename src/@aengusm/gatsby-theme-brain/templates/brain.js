c2c828fe5142e4b8f83e072a435188ae9b933f98
BaDi <baptiste.sene@gmail.com>
2021-10-17 22:10:42 +0200

    remove folder gatsy-theme-brain2

--- src/@aengusm/gatsby-theme-brain/templates/brain.js
+++ src/@aengusm/gatsby-theme-brain/templates/brain.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import BrainNote from '../components/BrainNote'
+import BrainTool from '../components/BrainTool'
 
 export default props => {
   return <BrainNote note={props.data.brainNote} />
@@ -33,6 +34,47 @@ export const query = graphql`
         } 
       }
       outboundReferenceNotes {
+        title
+        slug
+        childMdx {
+          excerpt(pruneLength: 280)
+        }
+      }
+    }
+  }
+`
+
+export default props => {
+  return <BrainTool tool={props.data.brainTool} />
+}
+
+export const query = graphql`
+  query BrainToolWithRefsBySlug($slug: String!) {
+    site {
+      ...site
+    }
+    brainTool(slug: { eq: $slug }) {
+      slug
+      title
+      childMdx {
+        body
+        frontmatter {
+          title
+          updated(formatString: "MMM DD, YYYY")
+          startDate(formatString: "MMM DD, YYYY")
+          slug
+          topics
+          growthStage
+        }
+      }
+      inboundReferenceTools {
+        title
+        slug
+        childMdx {
+          excerpt(pruneLength: 200)
+        } 
+      }
+      outboundReferenceTools {
         title
         slug
         childMdx {
