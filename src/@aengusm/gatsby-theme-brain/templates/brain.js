a8a4a73d10d67fecbca6fc2dbdc4ec7f379ce847
BaDi <baptiste.sene@gmail.com>
2021-10-18 22:48:22 +0200

    Update of brain.js

--- src/@aengusm/gatsby-theme-brain/templates/brain.js
+++ src/@aengusm/gatsby-theme-brain/templates/brain.js
@@ -46,39 +46,4 @@ export const query = graphql`
       }
     }
   }
-
-  // query BrainToolWithRefsBySlug($slug: String!) {
-  //   site {
-  //     ...site
-  //   }
-  //   brainTool(slug: { eq: $slug }) {
-  //     slug
-  //     title
-  //     childMdx {
-  //       body
-  //       frontmatter {
-  //         title
-  //         updated(formatString: "MMM DD, YYYY")
-  //         startDate(formatString: "MMM DD, YYYY")
-  //         slug
-  //         topics
-  //         growthStage
-  //       }
-  //     }
-  //     inboundReferenceTools {
-  //       title
-  //       slug
-  //       childMdx {
-  //         excerpt(pruneLength: 200)
-  //       } 
-  //     }
-  //     outboundReferenceTools {
-  //       title
-  //       slug
-  //       childMdx {
-  //         excerpt(pruneLength: 280)
-  //       }
-  //     }
-  //   }
-  // }
 `
